bab1faad5dcfb81a7c5d78fc08871f606c20ac6e
Almaz's Zefirka <almazbek.sheraliev321@gmail.com>
2025-04-22 00:47:15 +0600

    fix(deployment): fix deployment issue

--- src/app/router.ts
+++ src/app/router.ts
@@ -2,13 +2,16 @@ import Index from "@/pages/Index";
 import Settings from "@/pages/Settings";
 import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Index,
-  },
-  {
-    path: "/settings",
-    Component: Settings,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Index,
+    },
+    {
+      path: "/settings",
+      Component: Settings,
+    },
+  ],
+  { basename: import.meta.env.PROD && "/notes-app" }
+);
