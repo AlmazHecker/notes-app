ae74a581654f5981c15bd9b4100640e2d18ece07
Almaz's Zefirka <almazbek.sheraliev321@gmail.com>
2025-04-24 16:11:10 +0600

    feat(pwa): add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({ registerType: "autoUpdate", manifest }),
+  ],
   server: {
     port: 3000,
   },
@@ -20,3 +25,22 @@ export default defineConfig(({ mode }) => ({
   },
   base: mode === "gh-pages" ? "/notes-app/" : "/",
 }));
+
+const manifest = {
+  name: "Notes App",
+  short_name: "Notes App",
+  description: "Idk",
+  // theme_color: "#ffffff",
+  icons: [
+    {
+      src: "/android-chrome-192.png",
+      sizes: "192x192",
+      type: "image/png",
+    },
+    {
+      src: "/android-chrome-192.png",
+      sizes: "512x512",
+      type: "image/png",
+    },
+  ],
+};
