8a82a75fbca8242a83b1a7535080121e49aab232
Zefirka <almazbek.sheraliev321@gmail.com>
2025-11-23 17:01:35 +0600

    Updates

--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
 const CACHE_NAME = "cache-v2";
-const urlsToCache = ["/"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
   self.skipWaiting();
 });
 
